47397086ee419109382f48e954952a6766c7cab0
Mike Solomon <msolomon@thrillist.com>
2019-01-03 15:09:54 -0500

    send email if diff detected

--- zapier/index.js
+++ zapier/index.js
@@ -1,19 +1,20 @@
 const request = require('request')
 
 module.exports = {
-  sendDiffEmail: function(data) {
+  sendDiffEmail (changedRecords) {
     return new Promise(function(resolve, reject) {
-      buildTableForDiffEmail(data)
+      helpers.buildTableForDiffEmail(changedRecords)
         .then(table => {
           let options = {
             method: 'post',
-            body: JSON.stringify({table: table}), // this could be a problem
+            body: {table: table}, // this could be a problem
             json: true,
             url: process.env.ZAPIER_DIFF_EMAIL_ENDPOINT
           }
           
           request(options, function(err, response, body) {
             if (err) { console.log(err); return reject(err); }
+            console.log(body)
             return resolve(body)
           })
         })  
@@ -32,13 +33,13 @@ const helpers = {
             <th>Issue Record ID</th>
           </tr>
       `
-      
       for (const record of changedRecords) {
+        console.log(record)
         // make a row in the table for each record
         tableMarkup += `
           <tr>
             <td>${record.hrbp}</td>
-            <td><a hred="${record.url}"${record.id}</td>
+            <td><a href="${record.url}">${record.id}</a></td>
           </tr>
         `
       }
